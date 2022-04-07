4133ec3f1076e3f5462b4156636a84f74949c746
srluckychinnu <103173052+srluckychinnu@users.noreply.github.com>
2022-04-07 12:45:45 +0530

    Update recaptcha_v2.php
    upadat recaptcha

--- examples/recaptcha_v2.php
+++ examples/recaptcha_v2.php
@@ -4,7 +4,7 @@ set_time_limit(610);
 
 require(__DIR__ . '/../src/autoloader.php');
 
-$solver = new \TwoCaptcha\TwoCaptcha('YOUR_API_KEY');
+$solver = new \TwoCaptcha\TwoCaptcha('bd4edf72fb928f2b60a4058834e90f53');
 
 try {
     $result = $solver->recaptcha([
